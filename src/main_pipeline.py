0025a9bb2bba835e58d34020a5ff347e6277c512
Bart Kowal <bartkw12@gmail.com>
2025-10-27 13:27:36 -0400

    adding results and motion tracker to start setting up main pipeline.

--- src/main_pipeline.py
+++ src/main_pipeline.py
@@ -5,3 +5,10 @@ from sort.sort import *
 from util import get_car, read_license_plate, write_csv
 from config import video, output_csv
 
+# for writing to csv and saving info
+results = {}
+
+# object tracker used to track all the vehicles in the video
+mot_tracker = Sort()
+
+
