960200c46ea35b0f053dd21dd6093b572bdba4b3
Bart Kowal <bartkw12@gmail.com>
2025-10-28 18:03:24 -0400

    adding proper videos and data to config file.

--- src/config.py
+++ src/config.py
@@ -1,1 +1,8 @@
-# test
+video = 'ECE736_final2.mp4'
+output_csv = 'ECE736_final2_output.csv'
+
+# fill data
+output_csv_interpolated = 'ECE736_final2_output_interpolated.csv'
+
+# visualize
+output_video = './ECE736_final2_output.mp4'
