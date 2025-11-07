6a81fbbaec47aea42bd8c00404a4da98b7e9a084
Bart Kowal <160296648+bartkw12@users.noreply.github.com>
2025-11-07 12:47:32 -0500

    Update main_pipeline.py detect license plates in frames for loop

--- src/main_pipeline.py
+++ src/main_pipeline.py
@@ -45,3 +45,7 @@ while ret:
         track_ids = mot_tracker.update(np.asarray(detections_))  # all BB of all vehicles detected w tracking info
 
         # 4 detect license plates
+        license_plates = license_plate_detector(frame)[0]
+        for license_plate in license_plates.boxes.data.tolist():
+            x1, y1, x2, y2, score, class_id = license_plate
+
