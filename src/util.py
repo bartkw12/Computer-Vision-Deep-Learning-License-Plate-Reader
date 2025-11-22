56e53ee46cbac46c9ed5e5fa228ffed395b5a1ff
Bart Kowal <160296648+bartkw12@users.noreply.github.com>
2025-11-22 11:56:07 -0500

    Update util.py get car id in results

--- src/util.py
+++ src/util.py
@@ -34,8 +34,28 @@ def write_csv(results, output_path):
                                                 'license_number_score'))
 
         for frame_nmr in results.keys():
-                  for car_id in results[frame_nmr].keys():
-                      print(results[frame_nmr][car_id])
+            for car_id in results[frame_nmr].keys():
+                print(results[frame_nmr][car_id])
+                if 'car' in results[frame_nmr][car_id].keys() and \
+                   'license_plate' in results[frame_nmr][car_id].keys() and \
+                   'text' in results[frame_nmr][car_id]['license_plate'].keys():
+                    f.write('{},{},{},{},{},{},{}\n'.format(frame_nmr,
+                                                            car_id,
+                                                            '[{} {} {} {}]'.format(
+                                                                results[frame_nmr][car_id]['car']['bbox'][0],
+                                                                results[frame_nmr][car_id]['car']['bbox'][1],
+                                                                results[frame_nmr][car_id]['car']['bbox'][2],
+                                                                results[frame_nmr][car_id]['car']['bbox'][3]),
+                                                            '[{} {} {} {}]'.format(
+                                                                results[frame_nmr][car_id]['license_plate']['bbox'][0],
+                                                                results[frame_nmr][car_id]['license_plate']['bbox'][1],
+                                                                results[frame_nmr][car_id]['license_plate']['bbox'][2],
+                                                                results[frame_nmr][car_id]['license_plate']['bbox'][3]),
+                                                            results[frame_nmr][car_id]['license_plate']['bbox_score'],
+                                                            results[frame_nmr][car_id]['license_plate']['text'],
+                                                            results[frame_nmr][car_id]['license_plate']['text_score'])
+                            )
+        f.close()
 
 
 # ----------------------------------------------------------------------------------------------------------------------
