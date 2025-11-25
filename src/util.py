d4f3739aac397a7ff44075194aaf296d890efcf7
Bart Kowal <160296648+bartkw12@users.noreply.github.com>
2025-11-25 17:47:55 -0500

    Update util.py complete formatting license plate function.

--- src/util.py
+++ src/util.py
@@ -93,6 +93,15 @@ def format_license(text):
     Returns:
         str: Formatted license plate text.
     """
-    pass
+    license_plate_ = ''
+    mapping = {0: dict_int_to_char, 1: dict_int_to_char, 2: dict_int_to_char, 3: dict_int_to_char,
+               4: dict_char_to_int, 5: dict_char_to_int, 6: dict_char_to_int}
+    for j in [0, 1, 2, 3, 4, 5, 6]:
+        if text[j] in mapping[j].keys():
+            license_plate_ += mapping[j][text[j]]
+        else:
+            license_plate_ += text[j]
+
+    return license_plate_
 
 # also need a get car function
