049bce9c08f8e4b79215df745804b8c757409c54
Bart Kowal <160296648+bartkw12@users.noreply.github.com>
2025-11-29 22:15:44 -0500

    Update util.py finished get car function.

--- src/util.py
+++ src/util.py
@@ -138,4 +138,19 @@ def get_car(license_plate, vehicle_track_ids):
     Returns:
         tuple: Tuple containing the vehicle coordinates (x1, y1, x2, y2) and ID.
     """
-    pass
+    x1, y1, x2, y2, score, class_id = license_plate
+
+    foundIt = False
+    for j in range(len(vehicle_track_ids)):
+        xcar1, ycar1, xcar2, ycar2, car_id = vehicle_track_ids[j]
+
+        # verify that car bounding box contains license plate bounding box (for matching)
+        if x1 > xcar1 and y1 > ycar1 and x2 < xcar2 and y2 < ycar2:
+            car_indx = j
+            foundIt = True
+            break
+
+    if foundIt:
+        return vehicle_track_ids[car_indx]
+
+    return -1, -1, -1, -1, -1
