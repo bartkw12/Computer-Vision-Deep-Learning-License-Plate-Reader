6cf989f7ee33c0293ab67cafb55317eb28b6e3fe
Bart Kowal <160296648+bartkw12@users.noreply.github.com>
2025-12-02 13:30:13 -0500

    Update visualize.py start drawing borders

--- src/visualize.py
+++ src/visualize.py
@@ -7,5 +7,9 @@ import pandas as pd
 from config import video, output_csv_interpolated, output_video
 
 # need to draw borders
+def draw_border(img, top_left, bottom_right, color=(0, 171, 255), thickness=3, line_length_x=40, line_length_y=40):
+    x1, y1 = top_left
+    x2, y2 = bottom_right
 
 # need to input video
+
