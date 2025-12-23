21ca3e680fe4a1f9f54739f3d130ca25db5bc9e5
Bart Kowal <160296648+bartkw12@users.noreply.github.com>
2025-12-23 12:50:49 -0500

    Update fill_missing_data.py interpolate bounding boxes

--- src/fill_missing_data.py
+++ src/fill_missing_data.py
@@ -2,3 +2,7 @@ import csv
 import numpy as np
 from scipy.interpolate import interp1d
 from config import output_csv, output_csv_interpolated
+
+def interpolate_bounding_boxes(data):
+    # Extract necessary data columns from input data
+    frame_numbers = np.array([int(row['frame_nmr']) for row in data])
