d291a77f114b34c4dd22ae7efe76622776dfe669
Bart Kowal <160296648+bartkw12@users.noreply.github.com>
2025-12-24 16:53:15 -0500

    Update fill_missing_data.py cont. interpolating bounding boxes

--- src/fill_missing_data.py
+++ src/fill_missing_data.py
@@ -6,3 +6,7 @@ from config import output_csv, output_csv_interpolated
 def interpolate_bounding_boxes(data):
     # Extract necessary data columns from input data
     frame_numbers = np.array([int(row['frame_nmr']) for row in data])
+    car_ids = np.array([int(float(row['car_id'])) for row in data])
+    car_bboxes = np.array([list(map(float, row['car_bbox'][1:-1].split())) for row in data])
+    
+
