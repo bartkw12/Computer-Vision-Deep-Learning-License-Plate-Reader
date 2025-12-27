0eedb229685a82834c5409f1278da7d042fb8ce2
Bart Kowal <160296648+bartkw12@users.noreply.github.com>
2025-12-27 12:28:18 -0500

    Update fill_missing_data.py cont. fill missing data

--- src/fill_missing_data.py
+++ src/fill_missing_data.py
@@ -14,7 +14,11 @@ def interpolate_bounding_boxes(data):
     interpolated_data = []
     unique_car_ids = np.unique(car_ids)
     for car_id in unique_car_ids:
+
+        frame_numbers_ = [p['frame_nmr'] for p in data if int(float(p['car_id'])) == int(float(car_id))]
+        print(frame_numbers_, car_id)
     
+
 
 
 
