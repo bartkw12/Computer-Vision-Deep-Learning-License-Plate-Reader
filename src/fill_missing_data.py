aedf534a7ffdfae83068685d3e074e8f60f0455e
Bart Kowal <160296648+bartkw12@users.noreply.github.com>
2026-01-25 16:22:12 -0500

    Update fill_missing_data.py

--- src/fill_missing_data.py
+++ src/fill_missing_data.py
@@ -67,6 +67,11 @@ def interpolate_bounding_boxes(data):
                 original_row = [p for p in data if int(p['frame_nmr']) == frame_number and int(float(p['car_id'])) == int(float(car_id))][0]
                 row['license_plate_bbox_score'] = original_row['license_plate_bbox_score'] if 'license_plate_bbox_score' in original_row else '0'
                 row['license_number'] = original_row['license_number'] if 'license_number' in original_row else '0'
+                row['license_number_score'] = original_row['license_number_score'] if 'license_number_score' in original_row else '0'
+
+
+                
+
 
 
 
